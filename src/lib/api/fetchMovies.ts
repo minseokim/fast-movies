e65a10fd584713cf74ff87e4619d993770b72b6c
minseokim <minseoalexkim@gmail.com>
2020-12-03 16:28:33 +0900

    Add deploy scripts, update readme

--- src/lib/api/fetchMovies.ts
+++ src/lib/api/fetchMovies.ts
@@ -2,7 +2,7 @@ import { MovieSearchResult } from '../../typeDefs/MovieData';
 
 export const fetchMovies = async (searchQuery: string) => {
   const movieResult = await fetch(
-    `http://www.omdbapi.com/?s=${searchQuery}&apikey=${process.env.REACT_APP_API_KEY}`
+    `https://www.omdbapi.com/?s=${searchQuery}&apikey=${process.env.REACT_APP_API_KEY}`
   );
   const movieResultJson = await movieResult.json();
   const {
